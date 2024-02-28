3c7116f16af819f7993e87a2be610d6a0e797276
Srijan628162 <148005470+Srijan628162@users.noreply.github.com>
2024-02-28 22:11:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 27956463
+API_HASH     = "ab3cc18e5286dd5761e433de3d3e7b3c"
 BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
+SESSION      = "BQGqlO8AKJOVZFiTyP7m1-SN2Tq4isdf2J4b2my5OU_GdIkKEwC4H_thkp0hsmnx8NIDg6ssas5J-ahnXtFQ-BTEvOz_BXNXC6w5kEidIKFlCjphn88pNKp2NiPb9F1XaM-vLsxD2KCKUrxbVmtnc8LQJVwz0Wac4yA9IF_NLnYETekqPtc6UgpwRfQ-A9x6S26ak0LNHjFu-T-ht8Id9WBPr5hJn3avrtNXYjMwjUCRwekUbgcZbpgEaYT6FeoV7ryMxzY7cWBL4l4T8JJyTTUukHWL8x9RapnGtrjGH_Wzsm9OiJv9DQ6YLph7wJNRcaqNiVO6nCwm4PtGJh2NRPe-tZS6JgAAAAB0RLlJAA"
+DATABASE_URI = "mongodb+srv://sid628162:bgTwojGPtbBtRg0L@sdmovie.qx3o2yi.mongodb.net/?retryWrites=true&w=majority&appName=SDMovie"
+LOG_CHANNEL  = -1001853745008
+ADMIN        = 1950660937
 CHANNEL      = "@GreyMatter_Bots"
